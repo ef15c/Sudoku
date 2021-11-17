0b6fd078d58ea07f18e755f468921f71ff518ac8
ef15c <sphpn@free.fr>
2021-11-17 12:10:08 +0100

    Ajoutez des fichiers projet.

---
+++ SudokuInterfaceConsole/SudIntCo.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "SudSlvEn.h"
+
+void printSudoku(PtSudokuTable st)
+{
+	int i, j;
+
+	printf("%u\n%u\n", st->largeur, st->hauteur);
+	for (i = 0; i < st->largeur * st->hauteur; i++) {
+		for (j = 0; j < st->largeur * st->hauteur; j++)
+			printf("%u ", getSudokuSymbol(st, i, j));
+		printf("\n");
+	}
+}
+
+int __stdcall SolutionSudoku(PtSudokuTable st, int nbEssais, void* param)
+{
+	static int nbS = 0;
+
+	printf("\nSolution %u, %u essais :\n", ++nbS, nbEssais);
+	printSudoku(st);
+	return 0;
+}
+
+int main(int argc, char* argv[])
+{
+	PtSudokuTable st;
+	int cr, symbole;
+
+	printf("argc = %u\n", argc);
+	if (argc <= 1) {
+		st = newSudokuTable(3, 2);
+		if (st) {
+			printf("Cr‚ation d'un Sudoku 3x2 r‚ussie !\n");
+
+			cr = insertSudokuSymbol(st, 5, 1, 0);
+			printf("insertSudokuSymbol(st, 5, 1, 0) = %u\n", cr);
+			cr = insertSudokuSymbol(st, 5, 1, 0);
+			printf("insertSudokuSymbol(st, 5, 1, 0) = %u\n", cr);
+			cr = insertSudokuSymbol(st, 5, 0, -1);
+			printf("insertSudokuSymbol(st, 5, 0, -1) = %u\n", cr);
+			cr = insertSudokuSymbol(st, 5, -1, 0);
+			printf("insertSudokuSymbol(st, 5, -1, 0) = %u\n", cr);
+			cr = insertSudokuSymbol(st, 5, 6, 0);
+			printf("insertSudokuSymbol(st, 5, 6, 0) = %u\n", cr);
+			cr = insertSudokuSymbol(st, 5, 0, 6);
+			printf("insertSudokuSymbol(st, 5, 0, 6) = %u\n", cr);
+			cr = insertSudokuSymbol(st, -1, 1, 0);
+			printf("insertSudokuSymbol(st, -1, 0, 1) = %u\n", cr);
+			cr = insertSudokuSymbol(st, 7, 1, 0);
+			printf("insertSudokuSymbol(st, 7, 0, 1) = %u\n", cr);
+			cr = insertSudokuSymbol(st, 5, 1, 5);
+			printf("insertSudokuSymbol(st, 5, 1, 5) = %u\n", cr);
+			cr = insertSudokuSymbol(st, 5, 4, 0);
+			printf("insertSudokuSymbol(st, 5, 4, 0) = %u\n", cr);
+			cr = insertSudokuSymbol(st, 5, 0, 2);
+			printf("insertSudokuSymbol(st, 5, 0, 2) = %u\n", cr);
+			cr = insertSudokuSymbol(st, 4, 0, 2);
+			printf("insertSudokuSymbol(st, 4, 0, 2) = %u\n", cr);
+			cr = insertSudokuSymbol(st, 5, 2, 1);
+			printf("insertSudokuSymbol(st, 5, 2, 1) = %u\n", cr);
+
+			symbole = getSudokuSymbol(st, 2, 1);
+			printf("getSudokuSymbol(st, 2, 1) = %u\n", symbole);
+
+			symbole = getSudokuSymbol(st, 5, 5);
+			printf("getSudokuSymbol(st, 5, 5) = %u\n", symbole);
+
+			symbole = getSudokuSymbol(st, 6, 5);
+			printf("getSudokuSymbol(st, 6, 5) = %d\n", symbole);
+			releaseSudokuTable(st);
+			printf("Sudoku 3x2 d‚truit !\n");
+		}
+	}
+	else {
+		FILE* fs;
+		int h, l, i, j, s, cr;
+		errno_t fcr;
+
+		printf("argv[1]=%s\n", argv[1]);
+		fcr = fopen_s(&fs, argv[1], "r");
+		if (fcr) {
+			printf("Erreur d'ouverture du fichier %s\n", argv[1]);
+			return 1;
+		}
+		fscanf_s(fs, "%u", &l);
+		fscanf_s(fs, "%u", &h);
+		st = newSudokuTable(l, h);
+		if (st) {
+			printf("Cr‚ation d'un Sudoku %ux%u r‚ussie !\n",
+				st->largeur, st->hauteur);
+
+			for (i = 0; i < st->largeur * st->hauteur; i++)
+				for (j = 0; j < st->largeur * st->hauteur; j++) {
+					fscanf_s(fs, "%u", &s);
+					cr = insertSudokuSymbol(st, s, i, j);
+					if (cr) {
+						/* Erreur à l'insertion */
+						releaseSudokuTable(st);
+						return -1;
+					}
+				}
+			fclose(fs);
+
+			printSudoku(st);
+			solveSudoku(st, SolutionSudoku, NULL);
+
+			printf("Sudoku %ux%u",
+				st->largeur, st->hauteur);
+			releaseSudokuTable(st);
+			printf(" d‚truit !\n");
+		}
+	}
+	return 0;
+}
---
+++ SudokuSolver/SudSlvEn.c
@@ -0,0 +1,247 @@
+#include "SudSlvEn.h"
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+SUDOKU_SOLVER_DLLIMPORT PtSudokuTable __stdcall newSudokuTable (int largeur, int hauteur)
+{
+    PtSudokuTable st;
+    int i,j;
+    
+    if (largeur == 0 || hauteur == 0)
+        return 0;
+        
+    st = malloc(sizeof (SudokuTable));
+    if (!st)
+    	  return 0;
+    st->largeur = largeur;
+    st->hauteur = hauteur;
+    
+    /* Allocation de la mémoire de la table de vecteurs */
+    st->table = malloc(sizeof (int *) * largeur * hauteur);
+    if (!st->table) {
+			 free(st);
+			 return 0;
+    }
+    
+    for (i=0; i<largeur*hauteur; i++) {
+				st->table[i] = malloc(sizeof (int) * largeur * hauteur);
+				if (!st->table[i]) {
+					 for (j=0; j<i; j++)
+					 		 free(st->table[j]);
+		 		   free(st->table);
+		 		   free(st);
+		 		   return 0;
+			  }
+    }
+
+    for (i=0; i<largeur*hauteur; i++)
+    		for (j=0; j<largeur*hauteur; j++)
+    				st->table[i][j] = 0;
+    return st;
+}
+
+SUDOKU_SOLVER_DLLIMPORT void __stdcall releaseSudokuTable (PtSudokuTable st)
+{
+    int i;
+    
+    if (st) {
+	      if (st->table) {
+				    for (i=0; i<st->hauteur*st->largeur; i++)
+				 		    if (st->table[i])
+				 		 		    free(st->table[i]);
+				    free(st->table);
+	      }
+    		free(st);
+		}
+#ifdef CHECK_LEAK
+    lck_listAllocatedBlocks();
+#endif
+}
+
+static int isValid (PtSudokuTable st, int symbole,
+					int ligne, int colonne)
+{
+    int i, j, plr, pcr;
+
+    /* Le symbole ne doit pas être déjà présent sur la même ligne */
+    for (j=0; j<st->largeur*st->hauteur; j++)
+    		if (st->table[ligne][j] == symbole)
+            return 1;
+    /* Le symbole ne doit pas être déjà présent sur la même colonne */
+    for (i=0; i<st->largeur*st->hauteur; i++)
+    		if (st->table[i][colonne] == symbole)
+            return 2;
+    /* Le symbole ne doit pas être déjà présent dans la même région */
+    plr = (ligne/st->hauteur)*st->hauteur;
+    pcr = (colonne/st->largeur)*st->largeur;
+    
+    for (i=plr; i<plr+st->hauteur; i++)
+		    for (j=pcr; j<pcr+st->largeur; j++)
+    				if (st->table[i][j] == symbole)
+                return 3;
+
+    return 0;
+}
+
+SUDOKU_SOLVER_DLLIMPORT int __stdcall insertSudokuSymbol (PtSudokuTable st, int symbole,
+					int ligne, int colonne)
+{
+    int cr;
+    
+    if (ligne < 0
+     || colonne  < 0
+		 || ligne >= st->largeur*st->hauteur
+     || colonne  >= st->largeur*st->hauteur)
+     		return 10;
+     		
+    if (symbole < 0
+     || symbole  > st->largeur*st->hauteur)
+     		return 11;
+     		
+    /* Si le symbole est déjà dans la table, on ne fait rien et on sort */
+    if (st->table[ligne][colonne] == symbole)
+        return 0;
+        
+    /* Si le symbole vaut 0, il s'agit d'effacer un symbole de la table */
+    if (symbole == 0) {
+        st->table[ligne][colonne] = symbole;
+        return 0;
+    }
+    
+    /* Il faut insérer un symbole dans la table, on commence par vérifier si
+		   on a le droit d'insérer ce symbole à l'endroit demandé */
+		if ((cr = isValid(st, symbole, ligne, colonne)) != 0)
+			  return cr;
+				               
+    st->table[ligne][colonne] = symbole;
+    return 0;
+}
+
+SUDOKU_SOLVER_DLLIMPORT int __stdcall getSudokuSymbol (PtSudokuTable st,
+					int ligne, int colonne)
+{
+    if (ligne < 0
+     || colonne  < 0
+		 || ligne >= st->largeur*st->hauteur
+     || colonne  >= st->largeur*st->hauteur)
+     		return -1;
+     		
+ 		return st->table[ligne][colonne];
+}
+
+static int slvSud(PtSudokuTable st, PtSolvedActionFunction saf, int *tryVect,
+			 		 int *pnbe, void *param, int maxTry)
+{
+    int i, j, s, cr;
+    int nbPos, minNbPos, minLigne, minColonne;
+    
+    int *minTryVect;
+    
+    if (maxTry > 0 && *pnbe > maxTry)
+       return -2;
+       
+    minTryVect = malloc(sizeof (int) * st->largeur*st->hauteur);
+    
+    if (!minTryVect)
+    	 return -1;
+    	 
+    /* On recherche les cas pour lesquelles le nombre de possibilités est
+       minimal. S'il y en a plusieurs, on choisit la première */
+    minNbPos = st->hauteur*st->largeur+1;
+    for (i=0; i<st->hauteur*st->largeur; i++)
+    		for (j=0; j<st->hauteur*st->largeur; j++)
+						if (st->table[i][j] == 0) {
+					      /* La case est vide */
+							  nbPos = 0;
+							  for (s=1; s<=st->hauteur*st->largeur; s++)
+							 		  if (isValid(st, s, i, j) == 0)
+					 		          tryVect[nbPos++] = s;
+		            if (nbPos < minNbPos) {
+									  minNbPos = nbPos;
+									  for (s=0; s<nbPos; s++)
+									  		minTryVect[s] = tryVect[s];
+							  		minLigne = i;
+							  		minColonne = j;
+					      }
+				    }
+// Pour débug
+//    printf("minNbPos = %u [%u, %u] : ", minNbPos, minLigne, minColonne);
+//    for (s=0; s<minNbPos; s++)
+//    		printf("%u ", minTryVect[s]);
+//    printf("\n");
+// Fin débug
+    if (minNbPos == st->hauteur*st->largeur+1) {
+			  /* On a trouvé une nouvelle solution */
+			  cr = (*saf)(st, *pnbe, param);
+			  free (minTryVect);
+			  return cr;
+    }
+    
+    /* Il reste des symboles à placer dans la table, donc on essaie toutes
+       les combinaisons restantes */
+       
+    for (s=0; s<minNbPos; s++) {
+				st->table[minLigne][minColonne] = minTryVect[s];
+				(*pnbe)++;
+				cr = slvSud(st, saf, tryVect, pnbe, param, maxTry);
+				if (cr) {
+					  /* Il y a une erreur */
+						st->table[minLigne][minColonne] = 0;
+					  free (minTryVect);
+					  return cr;
+				}
+		} 
+		st->table[minLigne][minColonne] = 0;
+		free (minTryVect);
+		return 0;
+}
+
+SUDOKU_SOLVER_DLLIMPORT int __stdcall solveSudokuMaxTry (PtSudokuTable st,
+					PtSolvedActionFunction saf, void *param, int maxTry)
+/* maxTry : nombre maximum d'essais accordés pour tenter de résoudre un 
+   sudoku. Si maxTry vaut 0, il n'y a pas de limitation au nombre d'essais.*/
+{
+    int *tryVect;
+    int nbe=0;
+    int cr;
+    
+    tryVect = malloc(sizeof (int) * st->largeur*st->hauteur);
+    
+    if (!tryVect)
+    	 return -1;
+    	 
+    cr = slvSud(st, saf, tryVect, &nbe, param, maxTry);
+
+    free(tryVect);
+    return cr;
+}
+
+SUDOKU_SOLVER_DLLIMPORT void __stdcall solveSudoku (PtSudokuTable st, 
+					PtSolvedActionFunction saf, void *param)
+{
+      solveSudokuMaxTry(st, saf, param, 0);
+}
+
+BOOL APIENTRY DllMain (HMODULE hInst     /* Library instance handle. */ ,
+                       DWORD reason        /* Reason this function is being called. */ ,
+                       LPVOID reserved     /* Not used. */ )
+{
+    switch (reason)
+    {
+      case DLL_PROCESS_ATTACH:
+        break;
+
+      case DLL_PROCESS_DETACH:
+        break;
+
+      case DLL_THREAD_ATTACH:
+        break;
+
+      case DLL_THREAD_DETACH:
+        break;
+    }
+
+    /* Returns TRUE on success, FALSE on failure */
+    return TRUE;
+}
